161b0edb41cc2b5c6defb6fe83463a7ccca4d7d1
ahmetbatuhankilic <Erwelkf352!er>
2021-12-14 06:14:25 +0300

    *installed axios **make axios global property in store modules

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as plugins from '@/plugin'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(store).use(router).mount('#app')
+
+//app.config.globalProperties.$axios = plugins.axios
+store.$axios = plugins.axios
