dd29cb957f4cecdaae8610ae153872a2aef37809
ahmetbatuhankilic <Erwelkf352!er>
2021-12-17 23:31:34 +0300

    *add fontawesome for icons **add rubik font family and imported in app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 MenuAndRouterView
 </template>
 <script>
+import '@/assets/sass/index.sass'
 import '@/assets/css/bootstrap.min.css'
+import '@/assets/css/fontawesome-solid.min.css'
 // components
 import MenuAndRouterView from '@/layout/menu-and-router-view'
 export default {
